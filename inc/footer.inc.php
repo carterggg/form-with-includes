f3414a88e7f5a4ed1fd846efa080eceded43912c
Carter G <carterguthrie94@yahoo.com>
2014-12-01 21:29:36 -0500

    finished demo

---
+++ inc/footer.inc.php
@@ -0,0 +1,10 @@
+<footer>
+
+	<h3>Form Demo </h3>
+		<nav>
+			<a href="#" title="yo mama" class="home-lk">Home</a>
+			<a href="#" title="yo mama" class="about-lk">bout</a>
+			<a href="#" title="yo mama" class="current contact-lk">contact</a>
+		</nav>
+
+</footer>
---
+++ inc/header.inc.php
@@ -0,0 +1,10 @@
+<header>
+
+	<h1>Form Demo </h1>
+		<nav>
+			<a href="#" title="yo mama" class="home-lk">Home</a>
+			<a href="#" title="yo mama" class="about-lk">bout</a>
+			<a href="#" title="yo mama" class="current contact-lk">contact</a>
+		</nav>
+
+</header>
